c1dcceeca3d9a5b3ba152e1ba88d2984274fd89c
krishkumar-boom <parakrish28@gmail.com>
2026-03-04 20:34:02 +0530

    Create fetch-news.js

---
+++ scripts/fetch-news.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const Parser = require("rss-parser");
+
+const parser = new Parser();
+
+async function fetchNews() {
+  const feed = await parser.parseURL("https://pib.gov.in/rss.aspx");
+
+  const articles = feed.items.slice(0, 5).map(item => ({
+    title: item.title,
+    summary: item.contentSnippet,
+    source: "PIB",
+    date: new Date().toISOString().split("T")[0]
+  }));
+
+  fs.writeFileSync(
+    "./src/data/news.json",
+    JSON.stringify(articles, null, 2)
+  );
+}
+
+fetchNews();

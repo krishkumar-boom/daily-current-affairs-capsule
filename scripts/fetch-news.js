83c3831f1427fa085aa9de9b925857179652cc83
krishkumar-boom <parakrish28@gmail.com>
2026-03-05 00:28:25 +0530

    Update fetch-news.js

--- scripts/fetch-news.js
+++ scripts/fetch-news.js
@@ -4,19 +4,19 @@ import Parser from "rss-parser";
 const parser = new Parser();
 
 async function fetchNews() {
-  const feed = await parser.parseURL("https://pib.gov.in/RssMain.aspx?ModId=6&Lang=1&Regid=3");
 
-  const articles = feed.items.slice(0, 5).map(item => ({
-    title: item.title,
-    summary: item.contentSnippet,
-    source: "PIB",
-    date: new Date().toISOString().split("T")[0]
-  }));
+const feed = await parser.parseURL("https://pib.gov.in/RssMain.aspx?ModId=6&Lang=1&Regid=3");
 
-  fs.writeFileSync(
-    "./src/data/news.json",
-    JSON.stringify(articles, null, 2)
-  );
+const articles = feed.items.slice(0, 5).map(item => ({
+  title: item.title,
+  summary: item.contentSnippet,
+  source: "PIB",
+  date: new Date().toISOString().split("T")[0]
+}));
+
+fs.writeFileSync("src/data/news.json",
+JSON.stringify(articles, null, 2));
+
 }
 
 fetchNews();

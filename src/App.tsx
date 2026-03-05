1f3031dd29d0ddeaa65da080b8b38b7479659333
krishkumar-boom <parakrish28@gmail.com>
2026-03-05 11:10:35 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,37 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
 type News = {
-  title: string
-  summary: string
-  source: string
-  date: string
-}
+  title: string;
+  summary: string;
+  source: string;
+  date: string;
+};
 
-function App() {
+export default function App() {
 
-  const [news, setNews] = useState<News[]>([])
+  const [news, setNews] = useState<News[]>([]);
 
   useEffect(() => {
-
     fetch("/src/data/news.json")
       .then((res) => res.json())
       .then((data) => setNews(data))
-
-  }, [])
+      .catch((err) => console.log(err));
+  }, []);
 
   return (
-    <div style={{padding:"30px",fontFamily:"Arial"}}>
-
+    <div style={{ padding: "30px", fontFamily: "Arial" }}>
       <h1>Daily Current Affairs Capsule</h1>
 
       {news.map((item, index) => (
-        <div key={index} style={{
-          border:"1px solid #ddd",
-          padding:"20px",
-          marginBottom:"20px",
-          borderRadius:"10px"
-        }}>
-
+        <div
+          key={index}
+          style={{
+            border: "1px solid #ddd",
+            padding: "20px",
+            marginBottom: "20px",
+            borderRadius: "10px",
+          }}
+        >
           <h2>{item.title}</h2>
 
           <p>{item.summary}</p>
@@ -39,12 +39,8 @@ function App() {
           <small>
             {item.source} | {item.date}
           </small>
-
         </div>
       ))}
-
     </div>
-  )
-}
-
-export default App
+  );
+      }

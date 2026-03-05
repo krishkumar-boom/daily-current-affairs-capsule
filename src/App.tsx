1af89a1a557d9f2d760407692e2e6db9d06fb489
krishkumar-boom <parakrish28@gmail.com>
2026-03-05 11:12:30 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,40 +7,39 @@ type News = {
   date: string;
 };
 
-export default function App() {
+function App() {
 
   const [news, setNews] = useState<News[]>([]);
 
   useEffect(() => {
     fetch("/src/data/news.json")
-      .then((res) => res.json())
-      .then((data) => setNews(data))
-      .catch((err) => console.log(err));
+      .then(res => res.json())
+      .then(data => setNews(data));
   }, []);
 
   return (
-    <div style={{ padding: "30px", fontFamily: "Arial" }}>
+    <div style={{padding:"30px", fontFamily:"Arial"}}>
       <h1>Daily Current Affairs Capsule</h1>
 
-      {news.map((item, index) => (
-        <div
-          key={index}
-          style={{
-            border: "1px solid #ddd",
-            padding: "20px",
-            marginBottom: "20px",
-            borderRadius: "10px",
-          }}
-        >
-          <h2>{item.title}</h2>
+      {news.map((n,i)=>(
+        <div key={i} style={{
+          border:"1px solid #ddd",
+          padding:"20px",
+          marginBottom:"20px",
+          borderRadius:"10px"
+        }}>
 
-          <p>{item.summary}</p>
+          <h2>{n.title}</h2>
 
-          <small>
-            {item.source} | {item.date}
-          </small>
+          <p>{n.summary}</p>
+
+          <small>{n.source} | {n.date}</small>
+
         </div>
       ))}
+
     </div>
   );
-      }
+}
+
+export default App;

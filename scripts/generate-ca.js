9cb19eea4604b7591629966b0187abd05b3efdf3
krishkumar-boom <parakrish28@gmail.com>
2026-03-05 18:35:06 +0530

    Update generate-ca.js

--- scripts/generate-ca.js
+++ scripts/generate-ca.js
@@ -2,13 +2,15 @@ import fs from "fs";
 
 const news = JSON.parse(fs.readFileSync("public/news.json", "utf8"));
 
-const currentAffairs = news.map((item, index) => ({
+const currentAffairs = news.map((item) => {
+return {
 title: item.title,
 summary: item.summary,
 source: item.source,
 date: item.date,
 category: "General"
-}));
+};
+});
 
 fs.writeFileSync(
 "public/current-affairs.json",
